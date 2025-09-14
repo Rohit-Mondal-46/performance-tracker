49b1370b1fac3e80b29e7c1b1f693aae52e6f9a1
divineshiva003 <raishivam.in2k3@gmail.com>
2025-09-15 00:34:57 +0530

    Performance Logic added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,12 @@ const authRoutes = require('./auth');
 const adminRoutes = require('./admin');
 const organizationRoutes = require('./organization');
 const employeeRoutes = require('./employee');
+const performanceRoutes = require('./performance');
 
 router.use('/auth', authRoutes);
 router.use('/admin', adminRoutes);
 router.use('/organization', organizationRoutes);
 router.use('/employee', employeeRoutes);
+router.use('/performance', performanceRoutes);
 
 module.exports = router;
